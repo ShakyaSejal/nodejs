25510793a09aa479c7ceb776294bdabe5cd3f955
Sejal <sejalshakya72@gmail.com>
2024-08-20 10:51:54 +0545

    routing continue

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine' ,'ejs')
+                   
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.render("home.ejs");
 });
 app.get("/about", (req, res) => {
     res.send("this is about page");
